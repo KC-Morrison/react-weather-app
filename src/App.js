1ed7a1de6025bd7335665071ea242fc89e5dc91a
KMorrison <korpmorrison@gmail.com>
2022-11-22 09:13:27 +1100

    added github repo and personal site urls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ export default function App() {
 	return (
 		<div className="App">
 			<h1>Weather App</h1>
+			<footer>
+				<p>
+					This project was coded by{" "}
+					<a href="https://kmorrison.dev/" target="_blank" rel="noreferrer">
+						Katherine Morrison
+					</a>{" "}
+					and is
+				</p>
+				<a
+					href="https://github.com/KC-Morrison/react-weather-app"
+					target="_blank"
+					rel="noreferrer"
+				>
+					open-sourced on GitHub
+				</a>
+			</footer>
 		</div>
 	);
 }
