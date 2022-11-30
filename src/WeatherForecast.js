c7f9acf49248a976997d50376aedadf1838634a5
KMorrison <korpmorrison@gmail.com>
2022-11-30 21:12:49 +1100

    Create WeatherForecastDay component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
@@ -17,16 +18,7 @@ export default function WeatherForecast(props) {
 			<div className="WeatherForecast">
 				<div className="row">
 					<div className="col">
-						<div className="WeatherForecast-day">Thu</div>
-						<WeatherIcon code="01d" size={30} />
-						<div className="WeatherForecast-temperatures">
-							<span className="WeatherForecast-temperature-max">
-								{forecast[0].temp.max}{" "}
-							</span>
-							<span className="WeatherForecast-temperature-min">
-								{forecast[0].temp.min}
-							</span>
-						</div>
+						<WeatherForecastDay data={forecast[0]} />
 					</div>
 				</div>
 			</div>
