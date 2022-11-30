9312c9e0aabdcb2933f7a46a67bdf62f9eeb2f2c
KMorrison <korpmorrison@gmail.com>
2022-12-01 09:20:04 +1100

    add useEffect to update forecast on new city search

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
@@ -7,6 +6,11 @@ import axios from "axios";
 export default function WeatherForecast(props) {
 	let [loaded, setLoaded] = useState(false);
 	let [forecast, setForecast] = useState(false);
+
+	useEffect(() => {
+		setLoaded(false);
+	}, [props.coordinates]); //useEffect means that when we search for a new city, the forecast updates. Basically whenever the coordinates change, loaded is set to false which triggers the else in the if else statement
+
 	function handleResponse(response) {
 		setForecast(response.data.daily);
 		setLoaded(true);
@@ -17,9 +21,15 @@ export default function WeatherForecast(props) {
 		return (
 			<div className="WeatherForecast">
 				<div className="row">
-					<div className="col">
-						<WeatherForecastDay data={forecast[0]} />
-					</div>
+					{forecast.map(function (dailyForecast, index) {
+						if (index < 5) {
+							return (
+								<div className="col" key={index}>
+									<WeatherForecastDay data={dailyForecast} />
+								</div>
+							);
+						}
+					})}
 				</div>
 			</div>
 		);
